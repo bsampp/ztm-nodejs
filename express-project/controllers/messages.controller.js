16d31af219f4e919c600913c5fd661f5de96e107
Bruno Sampietro <b.sampietro02@gmail.com>
2023-11-19 17:19:46 -0300

    express project - templating engine

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,7 +1,11 @@
 const path = require('path')
 
 function getMessages(req, res){
-    res.sendFile(path.join(__dirname,'..', 'public', 'skimountain.jpg'))
+    res.render('messages', {
+        title: 'Messages',
+        friend: 'Bruno'
+    });
+    //res.sendFile(path.join(__dirname,'..', 'public/images', 'skimountain.jpg'))
 }
 
 function postMessage(req, res){
--- express-project/server.js
+++ express-project/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const messagesRouter = require('./routes/messages.router');
 const friendsRouter = require('./routes/friends.router');
 
 const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 const PORT = 3000;
 
@@ -14,8 +19,15 @@ app.use((req, res, next) => {
     console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
+app.use('/site', express.static('public'));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home Page',
+        caption: 'Testing templating engines'
+    });
+});
 
 app.use('/friends', friendsRouter);
 
