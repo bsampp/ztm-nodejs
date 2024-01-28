3744c10ebb0e17012a471a7efd5ef7e98a8781af
Bruno Sampietro <b.sampietro02@gmail.com>
2024-01-28 12:59:36 -0300

    npm scripts

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -13,3 +13,7 @@ app.use(express.json());
 app.use(planetsRouter);
 
 module.exports = app;
+
+async function startSerVER(){
+    skehnnsajdkhqwkjehqwehoiuq2y312oi3uy1oi3y1u2oi3u12o3iu123oi1u23oi12u3o1i2u3o1i2u31o2i3u1o2i3u12oi3u1o2i3
+}
